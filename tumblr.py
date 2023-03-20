abe651352df41032897dabba3d9dcbf771d89836
MaxwellChappell <60639983+MaxwellChappell@users.noreply.github.com>
2023-03-20 00:21:54 -0500

    get horse info

--- tumblr.py
+++ tumblr.py
@@ -2,12 +2,14 @@ import pytumblr
 import random
 import settings
 import re
+import requests
+from bs4 import BeautifulSoup
 
 
 def format_name(horse):
     # removes / in the URL and numbers at the end
     ret = re.sub("[\/]|\d+$", "", horse)
-    #removes + seperators
+    # removes + seperators
     ret = re.sub("\+", " ", ret)
     ret = ret.upper()
     return ret
@@ -21,6 +23,54 @@ def create_post(horse):
     client.create_text(blog, state="published", title=title, body=body)
 
 
+def get_info(horse):
+    url = "https://www.pedigreequery.com" + horse
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    results = soup.find_all("td", {"data-g": "1"})
+
+    sire = "Not Recorded"
+    dame = "Not Recorded"
+    for line in results:
+        if "f" in line["class"]:
+            dame = line.find("a").text
+        elif "m" in line["class"]:
+            sire = line.find("a").text
+
+    results = soup.find("font").text.split("\n")[0]
+
+    print(results)
+    year = re.search("\d\d\d\d[\?]*$", results)
+    if year:
+        year = year.group()
+    else:
+        year = "Year Not Recorded"
+
+    country = re.search("\(.*?\)", results)
+    if country:
+        country = country.group()[1:-1]
+    else:
+        country = "Country Not Recorded"
+
+    color = re.search(" [a-zA-Z\/a-zA-z]+\.", results)
+    print(color)
+    if color:
+        color = color.group()[1:]
+    else:
+        color = "Color Not Recorded"
+
+    sex = re.search(" [A-Z]+,", results)
+    if sex:
+        sex = sex.group()[1:-1]
+    else:
+        sex = "Sex Not Recorded"
+
+    story = f"{format_name(horse)} is a {color} {sex} born in {country} in {year}"
+    print(story)
+
+
 client = pytumblr.TumblrRestClient(
     settings.key,
     settings.key_secret,
@@ -30,7 +80,10 @@ client = pytumblr.TumblrRestClient(
 blog = settings.blog_name
 horse_list = settings.horse_list
 
-horse = random.choice(horse_list)
+for i in range(100):
+    horse = random.choice(horse_list)
 
-print(horse)
-create_post("/at+the+station2")
+    print(horse)
+    get_info(horse)
+    print()
+# create_post("/at+the+station2")
