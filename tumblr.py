2f4d5a930a6dd84546e806daa6d9da90318029fb
MaxwellChappell <60639983+MaxwellChappell@users.noreply.github.com>
2023-03-19 21:25:44 -0500

    initial commit

--- tumblr.py
+++ tumblr.py
@@ -1,2 +1,14 @@
 import pytumblr
 import sys
+import settings
+
+client = pytumblr.TumblrRestClient(
+    settings.key,
+    settings.key_secret,
+    settings.token,
+    settings.token_secret)
+
+blog = settings.blog_name
+
+client.create_quote(blog, state="published",
+                    quote="Hello, World!", source="testing tumblr api")
