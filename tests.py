00e478b80271984507819081fab2518b6dd984ce
MaxwellChappell <60639983+MaxwellChappell@users.noreply.github.com>
2023-03-20 22:33:42 -0500

    organize tests

---
+++ tests.py
@@ -0,0 +1,33 @@
+import horse_gen
+import settings
+import horse_gen
+import itertools
+import random
+import tumblr_bot
+
+def test_story():
+    name  = "test"
+    color = "bay"
+    sex = "mare"
+    country = "US"
+    year = 1984
+    sire = "dad"
+    dam = "mom"
+    fail = "Not Recorded"
+    
+    lst = list(itertools.product([0, 1], repeat=6))
+
+    for i in lst:
+        things = [color, sex, country, year, sire, dam]
+        things = [item if i[index]==1 else fail for index, item in enumerate(things)]
+        print(things,horse_gen.create_story(name, *things),"", sep="\n")
+
+def run_without_posting(n):
+    blog = settings.blog_name
+    horse_list = settings.horse_list
+    for i in range(n):
+        horse = random.choice(horse_list)
+        tumblr_bot.create_post(horse, False)
+        print()
+
+run_without_posting(100)
--- tumblr_bot.py
+++ tumblr_bot.py
@@ -4,15 +4,16 @@ import settings
 import horse_gen
 
 
-def create_post(horse):
+def create_post(horse, real=True):
     name, story, link = horse_gen.get_info(horse)
     title = f"Random Real Thoroughbred: {name}"
     body = f"{story}<hr><hr>Link to their pedigreequery page: <a href={link}>{link}</a>"
     if horse == "/at+the+station2":
         body += "<hr>Wow, this is my horse, what are the odds?"
-    print(name, story, link, sep="\n")
-    #client.create_text(blog, state="published", title=title, body=body, tags=[horse])
-
+    if real:
+        client.create_text(blog, state="published", title=title, body=body, tags=[horse])
+    else:
+        print(name, story, link, sep="\n")
 
 client = pytumblr.TumblrRestClient(
     settings.key,
@@ -22,7 +23,8 @@ client = pytumblr.TumblrRestClient(
 
 blog = settings.blog_name
 horse_list = settings.horse_list
-for i in range(100):
-    horse = random.choice(horse_list)
-    create_post(horse)
-    print()
+
+
+horse = random.choice(horse_list)
+create_post(horse)
+
