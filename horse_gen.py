e00afee11fa0b9d3e42c52ce348c7453fdfd6f32
MaxwellChappell <60639983+MaxwellChappell@users.noreply.github.com>
2023-03-20 22:22:10 -0500

    change order

--- horse_gen.py
+++ horse_gen.py
@@ -30,11 +30,60 @@ def get_info(horse):
 
     return name, story, url
 
+
+def find_parents(soup):
+    results = soup.find_all("td", {"data-g": "1"})
+    sire = "Not Recorded"
+    dame = "Not Recorded"
+    for line in results:
+        if "f" in line["class"]:
+            dame = line.find("a").text
+        elif "m" in line["class"]:
+            sire = line.find("a").text
+    return sire, dame
+
+
+def find_year(info):
+    year = re.search("\d\d\d\d[\?]*$", info)
+    if year:
+        return year.group()
+    else:
+        return "Not Recorded"
+
+
+def find_country(info, horse):
+    return find_trait("country", countries, "\(.*?\)", info, horse)
+
+
+def find_color(info, horse):
+    return find_trait("color", colors, " [a-zA-Z\/a-zA-z]+\.", info, horse)
+
+
+def find_sex(info, horse):
+    return find_trait("sex", sexes, " [A-Z]+,", info, horse)
+
+
+def find_trait(trait_category, d, regex, results, horse):
+    trait = re.search(regex, results)
+    if trait:
+        trait = trait.group()[1:-1]
+        try:
+            trait = d[trait]
+            return trait
+        except KeyError:
+            record_KeyError(trait_category, trait, results, horse)
+    return "Not Recorded"
+
+
+def record_KeyError(trait, missing_key, results, horse):
+    with open("dict_problems.txt", "a") as f:
+        f.write(f"\n{trait}: {missing_key} \n{results}\n{horse}\n")
+
 def create_story(name, color, sex, country, year, sire, dam):
     reject = "Not Recorded"
     if "Not Recorded" not in (name, color, sex, country, year, sire, dam):
         return f"{name} is a {color} {sex} born in {country} in {year}. By {sire} out of {dam}."
-   
+
     story = name
     if reject not in (color, sex):
         story += f" is a {color} {sex}"
@@ -62,52 +111,3 @@ def create_story(name, color, sex, country, year, sire, dam):
         story += f" By {sire}."
 
     return story
-
-
-def find_parents(soup):
-    results = soup.find_all("td", {"data-g": "1"})
-    sire = "Not Recorded"
-    dame = "Not Recorded"
-    for line in results:
-        if "f" in line["class"]:
-            dame = line.find("a").text
-        elif "m" in line["class"]:
-            sire = line.find("a").text
-    return sire, dame
-
-
-def find_year(info):
-    year = re.search("\d\d\d\d[\?]*$", info)
-    if year:
-        return year.group()
-    else:
-        return "Not Recorded"
-
-
-def find_trait(trait_category, d, regex, results, horse):
-    trait = re.search(regex, results)
-    if trait:
-        trait = trait.group()[1:-1]
-        try:
-            trait = d[trait]
-            return trait
-        except KeyError:
-            record_KeyError(trait_category, trait, results, horse)
-    return "Not Recorded"
-
-
-def find_country(info, horse):
-    return find_trait("country", countries, "\(.*?\)", info, horse)
-
-
-def find_color(info, horse):
-    return find_trait("color", colors, " [a-zA-Z\/a-zA-z]+\.", info, horse)
-
-
-def find_sex(info, horse):
-    return find_trait("sex", sexes, " [A-Z]+,", info, horse)
-
-
-def record_KeyError(trait, missing_key, results, horse):
-    with open("dict_problems.txt", "a") as f:
-        f.write(f"\n{trait}: {missing_key} \n{results}\n{horse}\n")
